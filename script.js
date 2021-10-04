216440534ee020d3e8f3cb256cfd46eeec748771
maneth <manethyethmin22@gmail.com>
2021-10-04 12:22:48 +0530

    Fix a bug in code

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+// DOM Elements
 const inputEl = document.getElementById('input');
 const imageEl = document.getElementById('image');
 
+// Global variables
 const images = [
   'images/hangman0.png',
   'images/hangman1.png',
@@ -27,48 +29,59 @@ const words = [
 ];
 
 let currentImageIndex = 0;
-
 let secretWord = words[Math.floor(Math.random() * words.length)];
 let userWord = '';
 
+// Generating the user word
 for (let i = 0; i < secretWord.length; i++) {
   userWord += '_';
 }
 
+redrawUserWord();
+redrawImage();
+
+// Display word
 function redrawUserWord() {
   inputEl.textContent = userWord;
 }
 
+// Display image
 function redrawImage() {
   imageEl.src = images[currentImageIndex];
 }
 
-redrawUserWord();
-
+// Event for keypress
 window.addEventListener('keydown', handleKeyPress);
 
+// Handling the keypress event
 function handleKeyPress(e) {
+  // Check if the key which user press is in the secret word or not
   if (secretWord.includes(e.key)) {
+    // Changing the userword
     for (let i = 0; i < secretWord.length; i++) {
-      if (!userWord.includes('_')) {
-        document.querySelector('.main').innerHTML = `
-        <div>
-            <h2 style="text-align: center;">You Won</h2><br>
-            <h4 style="text-align: center;">The word is ${secretWord}</h4>
-        </div>
-        `;
-        break;
-      }
       if (secretWord[i] == e.key) {
         userWord = userWord.split('');
         userWord[i] = e.key;
         userWord = userWord.join('');
       }
-      redrawUserWord();
+    }
+    // Redraw the word
+    redrawUserWord();
+    // Check if there isn't a _ in word
+    if (!userWord.includes('_')) {
+      document.querySelector('.main').innerHTML = `
+      <div>
+          <h2 style="text-align: center;">You Won</h2><br>
+          <h4 style="text-align: center;">The word is ${secretWord}</h4>
+      </div>
+      `;
     }
   } else {
+    // Increment image index
     currentImageIndex += 1;
+    // Redraw image
     redrawImage();
+    // Check if it's the last image
     if (currentImageIndex >= images.length - 1) {
       document.querySelector('.main').innerHTML = `
       <div>

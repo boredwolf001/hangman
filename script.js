48fc94357ee088969f5a0f1028ce1ed5857fd402
maneth <manethyethmin22@gmail.com>
2021-10-18 09:00:17 +0530

    Added a alert element

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // DOM Elements
-const inputEl = document.getElementById('input');
-const imageEl = document.getElementById('image');
-const retriesCountEl = document.getElementById('retries-left-count');
+const inputEl = document.getElementById('input')
+const imageEl = document.getElementById('image')
+const alertEl = document.getElementById('alert')
+const wrongLettersEl = document.getElementById('w-letters')
 
 // Global variables
 const images = [
@@ -12,7 +13,7 @@ const images = [
   'images/hangman4.png',
   'images/hangman5.png',
   'images/hangman6.png',
-];
+]
 const words = [
   'hangman',
   'developer',
@@ -27,47 +28,62 @@ const words = [
   'forest',
   'wallpaper',
   'typography',
-];
+]
 
-let currentImageIndex = 0;
-let secretWord = words[Math.floor(Math.random() * words.length)];
-let userWord = '';
+const guessedLetters = []
+const wrongLetters = []
+
+let currentImageIndex = 0
+let secretWord = words[Math.floor(Math.random() * words.length)]
+let userWord = ''
 
 // Generating the user word
 for (let i = 0; i < secretWord.length; i++) {
-  userWord += '_';
+  userWord += '_'
 }
 
-redrawUserWord();
-redrawImage();
+redrawUserWord()
+redrawImage()
 
 // Display word
 function redrawUserWord() {
-  inputEl.textContent = userWord;
+  inputEl.textContent = userWord
 }
 
 // Display image
 function redrawImage() {
-  imageEl.src = images[currentImageIndex];
+  imageEl.src = images[currentImageIndex]
+}
+
+function updateWLetters() {
+  wrongLettersEl.innerText = wrongLetters.join(',')
 }
 
 // Event for keypress
-window.addEventListener('keydown', handleKeyPress);
+window.addEventListener('keydown', handleKeyPress)
 
 // Handling the keypress event
 function handleKeyPress(e) {
+  if (guessedLetters.includes(e.key)) {
+    alertEl.classList.add('show')
+
+    setTimeout(() => alertEl.classList.remove('show'), 2000)
+    return
+  }
+  // Add to guessed letters
+  guessedLetters.push(e.key)
   // Check if the key which user press is in the secret word or not
   if (secretWord.includes(e.key)) {
     // Changing the userword
     for (let i = 0; i < secretWord.length; i++) {
       if (secretWord[i] == e.key) {
-        userWord = userWord.split('');
-        userWord[i] = e.key;
-        userWord = userWord.join('');
+        userWord = userWord.split('')
+        userWord[i] = e.key
+        userWord = userWord.join(', ')
       }
     }
     // Redraw the word
-    redrawUserWord();
+    redrawUserWord()
     // Check if there isn't a _ in word
     if (!userWord.includes('_')) {
       document.querySelector('.main').innerHTML = `
@@ -75,20 +91,24 @@ function handleKeyPress(e) {
           <h2 style="text-align: center;">You Won!</h1><br>
           <h4 style="font-weight: 300;text-align: center;">The word is ${secretWord}</h4>
       </div>
-      `;
+      `
     }
   } else {
+    // Add to wrong letters
+    wrongLetters.push(e.key)
     // Increasing image index
-    currentImageIndex += 1;
+    currentImageIndex += 1
     // Redraw image
-    redrawImage();
+    redrawImage()
+    // Update wrong leters
+    updateWLetters()
     // Check if it's the last image
     if (currentImageIndex >= images.length - 1) {
       document.querySelector('.main').innerHTML = `
       <div>
       <h2 style="text-align: center;">You Lose</h2><br>
       <h4 style="font-weight: 300;text-align: center;">The word is ${secretWord}</h4></div>
-      `;
+      `
     }
   }
 }
